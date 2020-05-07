6a9b41aa533c298df4b6ebce5dcf0bcc9bcade6b
Michael V <michaelvrld@gmail.com>
2020-05-07 18:01:00 +0200

    Create package

--- config/ytconverter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'download' => [
-        'max_length' => env('DOWNLOAD_MAX_LENGTH', 0),
-        'path' => storage_path('app/public') . '/'
-    ],
-    'search_max_results' => env('SEARCH_MAX_RESULTS', 10)
-];
---
+++ src/YoutubeAPIServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MichaelBelgium\YoutubeAPI;
+
+use Illuminate\Support\ServiceProvider;
+
+class YoutubeAPIServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        
+    }
+}
