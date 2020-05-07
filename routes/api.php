94fed5510222c62296a0412527ec7007e12d0d1e
Michael V <michaelvrld@gmail.com>
2020-05-07 22:32:43 +0200

    Use middleware api

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix' => config('youtube-api.route_prefix', 'ytconverter'),
-    'namespace' => 'MichaelBelgium\YoutubeAPI\Controllers'
+    'namespace' => 'MichaelBelgium\YoutubeAPI\Controllers',
+    'middleware' => 'api'
 ], function() {
     Route::post('/convert', 'ApiController@convert');
     Route::delete('/{id}', 'ApiController@delete');
