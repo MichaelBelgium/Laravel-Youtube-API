337e59c5cfce6aee423df080571daea879fdddd7
Michael V <michaelvrld@gmail.com>
2020-05-07 21:36:31 +0200

    Add route_prefix config

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'prefix' => 'ytconverter',
+    'prefix' => config('youtube-api.route_prefix', 'ytconverter'),
     'namespace' => 'MichaelBelgium\YoutubeAPI\Controllers'
 ], function() {
     Route::post('/convert', 'ApiController@convert');
