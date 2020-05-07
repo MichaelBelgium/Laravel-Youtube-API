8c5f0df5c669540b4119b6f8a382208f6d7ae72a
Michael V <michaelvrld@gmail.com>
2020-05-07 22:45:56 +0200

    Add config variables

--- config/youtube-api.php
+++ config/youtube-api.php
@@ -12,5 +12,12 @@ return [
      |
     */
     
-    'route_prefix' => 'ytconverter'
+    'route_prefix' => 'ytconverter',
+
+    'download' => [
+        'max_length' => 0,
+        'path' => storage_path('app/public') . '/'
+    ],
+
+    'search_max_results' => 10
 ];
