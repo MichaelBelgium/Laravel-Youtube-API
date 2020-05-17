2703d82d8fdc7ad36c593f4378a753bbb0dddbbc
Michael V <michaelvrld@gmail.com>
2020-05-17 20:14:30 +0200

    Add config documentation

--- config/youtube-api.php
+++ config/youtube-api.php
@@ -3,18 +3,36 @@
 return [
 
     /*
-     |--------------------------------------------------------------------------
-     | Route prefix
-     |--------------------------------------------------------------------------
-     |
-     |
-     |
-     |
+    |--------------------------------------------------------------------------
+    | Routing prefix
+    |--------------------------------------------------------------------------
+    |
+    | This value is the section of all the api routes after 'api/' 
+    | Example: api/ytconverter/convert
+    |
     */
     
     'route_prefix' => 'ytconverter',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Maximum download length
+    |--------------------------------------------------------------------------
+    |
+    | Specify in seconds what the maximum duration length can be for converting. Set to 0 to disable
+    |
+    */
+
     'download_max_length' => 0,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Maximum search results
+    |--------------------------------------------------------------------------
+    |
+    | Specify the maximum amount of results the search route returns
+    |
+    */
 
     'search_max_results' => 10
 ];
