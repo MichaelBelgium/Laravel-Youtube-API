99bc7e69c1b0c19cf080a42577fab6363ecb14de
Michael V <michaelvrld@gmail.com>
2021-10-06 14:59:15 +0200

    Use Http facade

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,12 +1,10 @@
 <?php
 namespace MichaelBelgium\YoutubeAPI\Controllers;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\RequestOptions;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use MichaelBelgium\YoutubeAPI\Models\Log;
 
 class HomeController extends Controller
@@ -18,23 +16,16 @@ class HomeController extends Controller
 
     public function onPost(Request $request)
     {
-        $client = new Client();
-        try {
-            $response = $client->post(route('youtube-api.convert'), [
-                RequestOptions::HEADERS => [
-                    'Accept' => 'application/json'
-                ],
-                RequestOptions::FORM_PARAMS => $request->all()
-            ]);
+        $response = Http::post(route('youtube-api.convert'), $request->all())->object();
 
-            return redirect()->back()->with('converted', json_decode($response->getBody()->getContents()));
-        } catch (ClientException $ex) {
-            $obj = json_decode($ex->getResponse()->getBody());
-            if(property_exists($obj, 'error_messages'))
-                return redirect()->back()->withErrors($obj->error_messages);
+        if($response->error) {
+            if(property_exists($response, 'error_messages'))
+                return redirect()->back()->withErrors($response->error_messages);
             else
-                return redirect()->back()->with('error', $obj->message);
+                return redirect()->back()->with('error', $response->message);
         }
+        
+        return redirect()->back()->with('converted', $response);
     }
 
     public function logs(Request $request)
