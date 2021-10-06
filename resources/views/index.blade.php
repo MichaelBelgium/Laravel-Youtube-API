7df80aa979a593578d8579b5544d622f9d32f1e3
Michael V <michaelvrld@gmail.com>
2021-10-06 11:18:10 +0200

    Update to bootstrap 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Youtube converter</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 </head>
 <body>
     <div class="p-2">
         <div class="card">
             <div class="card-header">
-                <h5 class="card-title">Youtube converter</h5>
+                <h5 class="card-title">Convert</h5>
             </div>
             <div class="card-body">
                 @if (session('error'))
@@ -21,18 +21,19 @@
                 
                 <form action="{{ route('youtube-api.submit') }}" method="post" id="frm-convert">
                     @csrf
-                    <div class="form-group">
+                    <div class="form-floating mb-3">
                         <input type="text" name="url" class="form-control @error('url') is-invalid @enderror" id="url" placeholder="Youtube url" required value="{{ old('url') }}"/>
+                        <label for="url">Youtube url</label>
                         @error('url')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
-                    <div class="form-group">
-                        <label for="format">Format</label>
+                    <div class="form-floating mb-3">
                         <select class="form-control @error('format') is-invalid @enderror" name="format" id="format">
                             <option value="mp3">Audio (mp3)</option>
                             <option value="mp4">Video (mp4)</option>
                         </select>
+                        <label for="format">Format</label>
                         @error('format')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -42,7 +43,7 @@
                             <input class="form-control" type="text" name="api_token" placeholder="API token" required value="{{ request()->token }}">
                         </div>
                     @endif
-                    <button type="submit" class="btn btn-outline-primary"><i class="fa fa-refresh" aria-hidden="true"></i> Convert</button>
+                    <button type="submit" class="btn btn-outline-primary"><i class="fas fa-sync-alt"></i> Convert</button>
                 </form>
             </div>
         </div>
@@ -53,7 +54,7 @@
                 <h5 class="card-title">Json response</h5>
             </div>
             <div class="card-body">
-                <pre>@json(session('converted'))</pre>
+                <pre>@json(session('converted'), JSON_PRETTY_PRINT)</pre>
             </div>
             <div class="card-footer">
                 <table class="table table-borderless table-sm w-auto">
@@ -88,22 +89,25 @@
                             <td>Uploaded at</td>
                             <td>{{ session('converted')->uploaded_at->date }}</td>
                         </tr>
+                        <tr>
+                            <td>
+                                <a target="_blank" class="btn btn-outline-primary" href="{{ session('converted')->file }}"><i class="fas fa-download"></i> Listen/download</a>
+                                <form action="{{ route('youtube-api.delete', ['id' => session('converted')->youtube_id]) }}" method="post" class="d-inline-block">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-outline-danger"><i class="fa fa-trash" aria-hidden="true"></i> Remove file</button>
+                                </form>
+                            </td>
+                            <td></td>
+                        </tr>
                     </tbody>
                 </table>
-                <a target="_blank" class="btn btn-outline-primary" href="{{ session('converted')->file }}"><i class="fa fa-cloud-download" aria-hidden="true"></i> Listen/download</a>
-
-                <form action="{{ route('youtube-api.delete', ['id' => session('converted')->youtube_id]) }}" method="post">
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit" class="btn btn-outline-danger"><i class="fa fa-trash" aria-hidden="true"></i> Remove file</button>
-                </form>
             </div>
         </div>
         @endif
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
